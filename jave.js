b6a2119585a2ab04a7ec98ce1f7c5b370e549b38
Gideon Farrell <gideon@Gidbook.local>
2013-04-06 00:43:38 +0100

    Refactored jave as an object literal with methods, because otherwise the `this` keyword doesn't work properly without making it a fresh class and so on.

--- jave.js
+++ jave.js
@@ -1,89 +1,99 @@
 define(['jquery', 'jave_api'], function($, JaveAPI) {
-    var behaviours = {},
-    jave       = function(options) {
-        var defaults = {
-            selector: '[data-behaviour]',
-            auto:     true,
-            root:     document.body
-        };
+    $.jave = {
+        /**
+         * Dictionary of behaviours
+         * @type {Object}
+         */
+        _behaviours: {},
 
-        this._behaviours = {};  // dictionary of behaviours
+        init: function(options) {
+            var defaults = {
+                selector: '[data-behaviour]',
+                auto:     true,
+                root:     document.body
+            };
 
-        this.options = $.merge(defaults, options);
+            this.options = (typeof options == 'object') ? $.merge(defaults, options) : defaults;
 
-        if(this.options.auto) this.run();
-    };
+            if(this.options.auto) this.run();
+        },
 
-    $.jave = jave;  // define as an extension of jQuery
-
-    /**
-     * Define a behaviour
-     * @param  {string}   name the data-behaviour attribute (behaviour name)
-     * @param  {Function} fn   the function that will be run. Should take ($el, api) as arguments (see README for more info).
-     * @return {void}
-     */
-    jave.define = function(name, fn) {
-        if(typeof fn == 'function') {
-            if(!this._behaviours.hasOwnProperty(name)) {
-                this._behaviours[name] = fn;
+        /**
+         * Define a behaviour
+         * @param  {string}   name the data-behaviour attribute (behaviour name)
+         * @param  {Function} fn   the function that will be run. Should take ($el, api) as arguments (see README for more info).
+         * @return {void}
+         */
+        define: function(name, fn) {
+            if(typeof fn == 'function') {
+                if(!this._behaviours.hasOwnProperty(name)) {
+                    this._behaviours[name] = fn;
+                } else {
+                    throw new Error('Jave: behaviour "' + name + '" is already defined.');
+                }
             } else {
-                throw new Error('Jave: behaviour "' + name + '" is already defined.');
+                throw new Error('Jave: behaviour definitions must be functions ("' + name + '").');
             }
-        } else {
-            throw new Error('Jave: behaviour definitions must be functions ("' + name + '").');
-        }
-    };
+        },
 
-    /**
-     * Runs Jave, optionally on a particular element containing behaving elements.
-     * @param  {jQuery} $root the root element containing elements with behaviours.
-     * @return {void}
-     */
-    jave.run = function($root) {
-        if($root === undefined || $root === null) {
-            $root = $(this.options.root);
-        } 
+        /**
+         * Runs Jave, optionally on a particular element containing behaving elements.
+         * @param  {jQuery} $root the root element containing elements with behaviours.
+         * @return {void}
+         */
+        run: function($root) {
+            if($root === undefined || $root === null) {
+                $root = $(this.options.root);
+            } 
 
-        var children = $root.find(this.options.selector);
-        $.each(children, function(i, $el) {
-            $.jave.process($el);
-        });
-    };
+            var children = $root.find(this.options.selector);
+            $.each(children, function(i, el) {
+                $.jave.process($(el));
+            });
+        },
 
-    /**
-     * Process an element and apply behaviours.
-     * @param  {jQuery} $el the element to process
-     * @return {void}
-     */
-    jave.process = function($el) {
-        var applied_behaviours  = $el.data('applied-behaviours'),
-            behaviours          = $el.data('behaviour').split(' ');
+        /**
+         * Process an element and apply behaviours.
+         * @param  {jQuery} $el the element to process
+         * @return {void}
+         */
+        process: function($el) {
+            var applied_behaviours  = $el.data('applied-behaviours'),
+                behaviours          = $el.data('behaviour').split(' ');
 
-        if(typeof applied_behaviours != 'object') {
-            applied_behaviours = [];
-        }
-
-        $.each(behaviours, function(i, b) {
-            if(applied_behaviours.contains(b) !== true) {
-                $.jave.__makeBehave($el, b);
-
-                applied_behaviours.push(b);
-                $el.data('applied-behaviours', applied_behaviours);
+            if(typeof applied_behaviours != 'object') {
+                applied_behaviours = [];
             }
-        });
+
+            $.each(behaviours, function(i, b) {
+                if(applied_behaviours.indexOf(b) !== -1) {
+                    $.jave.__makeBehave($el, b);
+
+                    applied_behaviours.push(b);
+                    $el.data('applied-behaviours', applied_behaviours);
+                }
+            });
+        },
+
+        /**
+         * Make an element use a behaviour.
+         * @param  {jQuery} $el       the element in question.
+         * @param  {string} behaviour the behaviour name.
+         * @return {void}
+         */
+        __makeBehave: function($el, behaviour) {
+            var api = new JaveAPI($el, behaviour);
+            if (!this._behaviours.hasOwnProperty(behaviour)) {
+                throw new Error('Jave: no such behaviour "' + behaviour + '" has been defined.');
+            }
+            this._behaviours[behaviour].call(this, $el, api);
+        }
     };
 
-    /**
-     * Make an element use a behaviour.
-     * @param  {jQuery} $el       the element in question.
-     * @param  {string} behaviour the behaviour name.
-     * @return {void}
-     */
-    jave.__makeBehave = function($el, behaviour) {
-        var api = new JaveAPI($el, behaviour);
-        if (!this._behaviours.hasOwnProperty(behaviour)) {
-            throw new Error('Jave: no such behaviour "' + behaviour + '" has been defined.');
+    // define shortcut as a jQuery plugin
+    $.fn.extend({
+        jave: function() {
+            $.jave.process($(this));
         }
-        this._behaviours[behaviour].call(this, $el, api);
-    };
+    });
 });
